05af09257b2b3a4a9f9b3ead1ef98b1be15fd4d8
Skillitronic <dosynkirill@gmail.com>
2024-05-15 22:00:12 +0300

    Create CreateEntityCommand.cs

---
+++ Arch.Buffer/EntityCommands/CreateEntityCommand.cs
@@ -0,0 +1,25 @@
+﻿using Arch.Core;
+using Arch.Core.Extensions;
+using Arch.Core.Utils;
+
+namespace Arch.CommandBuffer.EntityCommands
+{
+    public class CreateEntityCommand : IBufferCommand
+    {
+        private readonly World _world;
+        private readonly ComponentType[] _types;
+        private readonly object[] _components;
+
+        public CreateEntityCommand(World world, ComponentType[] types, object[] components)
+        {
+            _world = world;
+            _types = types;
+            _components = components;
+        }
+
+        public void Execute()
+        {
+            _world.Create(_types).SetRange(_components);
+        }
+    }
+}
